51f1010d264cd069d3c60ecf76a7a95a35cc1275
RomanB54 <r.barsukov@mgmnt.ru>
2025-07-01 14:57:47 +0300

    add readme

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,11 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import type { Configuration } from 'webpack';
+import type { Configuration as WebpackConfiguration } from 'webpack';
+import type { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const config: Configuration = {
   mode: 'development',
