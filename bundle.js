a53d35a7e988ac2abbc4f24bf0d0208589039785
RomanB54 <RomanB54@users.noreply.github.com>
2025-07-01 12:20:52 +0000

    deploy: c01c75852d668655f6e6e7200efdedad1943d952

--- bundle.js
+++ bundle.js
@@ -754,7 +754,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "./";
+/******/ 		__webpack_require__.p = "/FinalProjectApp/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
