5b5d5609d5271d02f9f507695f0440ab0af898cb
RomanB54 <r.barsukov@mgmnt.ru>
2025-07-01 14:42:53 +0300

    change webpack config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const OPENWEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
-export const IPINFO_TOKEN = process.env.REACT_APP_IPINFO_TOKEN;
-export const YANDEX_MAPS_API_KEY = process.env.REACT_APP_YANDEX_MAPS_API_KEY;
+export const OPENWEATHER_API_KEY = '50e9562e52bdc95310309ebf4c74c77c';
+export const IPINFO_TOKEN = '7ce0407bb7be70';
+export const YANDEX_MAPS_API_KEY = 'e091f93b-1d71-4a7d-ae59-8369de3754d8';
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,7 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import type { Configuration as WebpackConfiguration } from 'webpack';
-import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
-import webpack from 'webpack';
-import 'dotenv/config';
+import type { Configuration } from 'webpack';
 
-require('dotenv').config();
-interface Configuration extends WebpackConfiguration {
-  devServer?: DevServerConfiguration;
-}
 const config: Configuration = {
   mode: 'development',
   entry: './src/index.tsx',
@@ -34,27 +27,13 @@ const config: Configuration = {
       { test: /\.(png|jpe?g|gif)$/i, use: ['file-loader'] },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'public/index.html' }),
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_OPENWEATHER_API_KEY': JSON.stringify(
-        process.env.REACT_APP_OPENWEATHER_API_KEY,
-      ),
-      'process.env.REACT_APP_IPINFO_TOKEN': JSON.stringify(
-        process.env.REACT_APP_IPINFO_TOKEN,
-      ),
-      'process.env.REACT_APP_YANDEX_MAPS_API_KEY': JSON.stringify(
-        process.env.REACT_APP_YANDEX_MAPS_API_KEY,
-      ),
-    }),
-  ],
+  plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' })],
   devServer: {
     historyApiFallback: true,
     static: {
       directory: path.join(__dirname, 'dist'),
       publicPath: '/',
     },
-    port: 3000,
     open: true,
   },
 };
